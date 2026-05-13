751941786d5160cce92914f0364dbcc0d4fdb89c
Mylysh <maya.alysha.mohd@student.mmu.edu.my>
2026-05-13 12:49:17 +0800

    update bmi calculator

--- tuto12.cpp
+++ tuto12.cpp
@@ -14,6 +14,31 @@ bmi = weight/(height*height);
 
 cout << "Your bmi = " << bmi << endl;
 
+if (bmi < 18.5){
+    cout << "Your weight category is : Underweight" << endl;
+}
+
+else if (bmi >= 18.5 && bmi <= 24.9){
+    cout << "Your weight category is : Normal" << endl; 
+}
+
+else if (bmi >= 25 && bmi <= 29.9){
+    cout << "Your weight category is : Overweight" << endl; 
+}
+
+else if (bmi >= 30 && bmi <= 34.9){
+    cout << "Your weight category is : Medically Obese I" << endl;
+
+}
+
+else if (bmi >= 35 && bmi <= 39.9){
+    cout << "Your weight category is : Medically Obese II" << endl; 
+}
+
+else {
+    cout << "Your weight category is : Medically Obese III (Extreme Obesity)" << endl; 
+
+}
 
     return 0;
 }
