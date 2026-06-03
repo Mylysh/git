d60269e4841a66af8d8771ba011a4f16bb1958c4
Mylysh <maya.alysha.mohd@student.mmu.edu.my>
2026-06-04 03:44:29 +0800

    tuto 51

--- tuto51.cpp
+++ tuto51.cpp
@@ -1,52 +1,35 @@
 #include <iostream>
 using namespace std;
 
-class Fraction {
-private:
-    int numerator;
-    int denominator;
+class Fraction{
+    private:
+    int numerator, denominator;
 
-public:
-    Fraction(int num = 0, int den = 1) {
+    public:
+    Fraction(int num = 0, int denom = 1){
         numerator = num;
-        denominator = den;
-    }
+        denominator = denom;
+    };
 
-    Fraction add(Fraction f) {
-        int newNumerator   = (numerator * f.denominator) + (f.numerator * denominator);
-        int newDenominator = denominator * f.denominator;
-        return Fraction(newNumerator, newDenominator);
-    }
+    Fraction add(Fraction f){
+        int newNum = (numerator * f.denominator) + (f.numerator * denominator);
+        int newDenom = (denominator * f.denominator);
 
-    Fraction multiply(Fraction f) {
-        int newNumerator   = numerator   * f.numerator;
-        int newDenominator = denominator * f.denominator;
-        return Fraction(newNumerator, newDenominator);
-    }
+        return Fraction (newNum, newDenom);
 
-    void display() {
+    };
+
+    Fraction multiply (Fraction f){
+        int newNum = (numerator * f.numerator);
+        int newDenom = (denominator * f.denominator);    
+        return Fraction (newNum, newDenom);
+
+    };
+
+    void display(){
         cout << numerator << "/" << denominator << endl;
-    }
+    };
+
+
 };
-
-int main() {
-    Fraction f1(3, 4);
-    Fraction f2(1, 2);
-    Fraction result;
-
-    cout << "f1: ";
-    f1.display();
-
-    cout << "f2: ";
-    f2.display();
-
-    cout << "f1 + f2: ";
-    result = f1.add(f2);
-    result.display();
-
-    cout << "f1 * f2: ";
-    result = f1.multiply(f2);
-    result.display();
-
-    return 0;
-}
+   
